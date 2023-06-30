4add3bb56d5c074542ff4181647d032600f7b289
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-06-30 11:09:37 -0500

    Added unit tests for the PettingZoo parallel API.

--- patrolling_zoo/test/patrolling_zoo.py
+++
@@ -1,8 +0,0 @@
-from ..patrolling_zoo_v0 import PatrolGraph, parallel_env
-
-from pettingzoo.test import parallel_api_test
-
-if __name__ == "__main__":
-    graph = PatrolGraph("patrolling_zoo/env/cumberland.graph")
-    env = parallel_env(graph, num_agents=2)
-    parallel_api_test(env, num_cycles=1000)
---
+++ test/test_patrolling_zoo.py
@@ -0,0 +1,13 @@
+import unittest
+from pettingzoo.test import parallel_api_test
+from patrolling_zoo.patrolling_zoo_v0 import PatrolGraph, parallel_env
+
+class TestEnvironment(unittest.TestCase):
+
+    def test_parallel_api(self):
+        graph = PatrolGraph("patrolling_zoo/env/cumberland.graph")
+        env = parallel_env(graph, num_agents=2)
+        parallel_api_test(env, num_cycles=1000)
+
+if __name__ == '__main__':
+    unittest.main()
