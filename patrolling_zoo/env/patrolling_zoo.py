db4182ced91ad6c829d43eb58a1079d22901a166
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-07-13 11:35:17 -0500

    Revert to old movement formula.

--- patrolling_zoo/env/patrolling_zoo.py
+++ patrolling_zoo/env/patrolling_zoo.py
@@ -366,10 +366,9 @@ class parallel_env(ParallelEnv):
         reached = distCurrToNext <= stepSize
         step = distCurrToNext if reached else stepSize
         if distCurrToNext > 0.0:
-            slope = (posNextNode[1] - agent.position[1]) / (posNextNode[0] - agent.position[0])
             agent.position = (
-                agent.position[0] + np.cos(np.arctan(slope)) * step,
-                agent.position[1] + np.sin(np.arctan(slope)) * step
+                agent.position[0] + (posNextNode[0] - agent.position[0]) * step / distCurrToNext,
+                agent.position[1] + (posNextNode[1] - agent.position[1]) * step / distCurrToNext
             )
         
         # Set information about the edge which the agent is currently on.
