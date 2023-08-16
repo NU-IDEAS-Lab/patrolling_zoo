aa1122851470276e2b0c0580f42ea7ce51f2fd93
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-08-16 11:08:31 -0500

    Fixed action and done translation in MAPPO PZ wrapper.

--- onpolicy/envs/patrolling/Patrolling_Env.py
+++ onpolicy/envs/patrolling/Patrolling_Env.py
@@ -76,6 +76,8 @@ class PatrollingEnv(object):
         return obs
 
     def step(self, action):
+
+        action = {self.env.possible_agents[i]: action[i] for i in range(self.num_agents)}
         obs, reward, done, trunc, info = self.env.step(action)
         obs = self._obs_wrapper(obs)
         reward = [reward[a] for a in self.env.possible_agents]
@@ -83,7 +85,7 @@ class PatrollingEnv(object):
             global_reward = np.sum(reward)
             reward = [[global_reward]] * self.num_agents
 
-        done = np.array([done] * self.num_agents)
+        done = [done[a] for a in self.env.possible_agents]
         info = self._info_wrapper(info)
         return obs, reward, done, info
 
