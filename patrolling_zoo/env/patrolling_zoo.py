b05f02a4a3ddc4f128efceb01a678e208a0406f6
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-08-17 14:41:09 -0500

    Give terminal reward based on average, not worst, idleness time.

--- patrolling_zoo/env/patrolling_zoo.py
+++ patrolling_zoo/env/patrolling_zoo.py
@@ -416,8 +416,8 @@ class parallel_env(ParallelEnv):
         if self.max_cycles >= 0 and self.step_count >= self.max_cycles:
             # Provide an end-of-episode reward.
             for agent in self.agents:
-                reward_dict[agent] += self.beta * self.max_cycles / (self.pg.getWorstIdlenessTime(self.step_count) + 1e-8)
-                # reward_dict[agent] += 10000.0 / self.pg.getAverageIdlenessTime(self.step_count)
+                # reward_dict[agent] += self.beta * self.max_cycles / (self.pg.getWorstIdlenessTime(self.step_count) + 1e-8)
+                reward_dict[agent] += self.beta * self.max_cycles / self.pg.getAverageIdlenessTime(self.step_count)
                 # reward_dict[agent] /= self._minMaxNormalize(self.pg.getWorstIdlenessTime(self.step_count), minimum=0.0, maximum=self.max_cycles)
             
             truncated_dict = {a: True for a in self.agents}
