1e83f5da1750414d3d904153ca63afcd1acb0daf
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2023-09-21 18:05:01 -0500

    Added random heuristic.

---
+++ algorithm/random.py
@@ -0,0 +1,63 @@
+import networkx as nx
+import numpy as np
+from IPython.display import clear_output
+import random
+
+from .algorithm import BaseAlgorithm
+
+
+class RandomChoice(BaseAlgorithm):
+    ''' This algorithm implements an idleness-based greedy heuristic for the patrolling problem. '''
+
+    def save(self, *args, **kwargs):
+        ''' Saves the model. '''
+        pass
+
+
+    def train(self, *args, seed=None, **kwargs):
+        ''' Trains the model. '''
+        pass
+
+    
+    def _reset(self):
+        pass
+
+
+    def evaluate(self, render=False, max_cycles=None, max_episodes=1, seed=None):
+        ''' Evaluates the model. '''
+        
+        if max_cycles != None:
+            self.env.max_cycles = max_cycles
+
+        for episode in range(max_episodes):
+            obs, info = self.env.reset(seed=seed)
+            self._reset()
+
+            if render:
+                clear_output(wait=True)
+                self.env.render()
+            
+            terms = [False]
+            truncs = [False]
+            while not any(terms) and not any(truncs):
+                actions = self.generateActions(obs)
+                obs, rewards, terms, truncs, infos = self.env.step(actions)
+
+                terms = [terms[a] for a in terms]
+                truncs = [truncs[a] for a in truncs]
+
+                if render:
+                    clear_output(wait=True)
+                    self.env.render()
+
+
+    def generateActions(self, obs):
+        ''' Generates a dictionary of actions. '''
+        
+        actions = {}
+
+        for agent in self.env.agents:
+            targetNode = random.randint(0, len(self.env.pg.graph.nodes) - 1)
+            actions[agent] = targetNode
+        
+        return actions
