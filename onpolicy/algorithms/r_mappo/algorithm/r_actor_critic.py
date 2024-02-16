Fixed agent masking in `forward` method of actor.

onpolicy/algorithms/r_mappo/algorithm/r_actor_critic.py
@@ -12,6 +12,8 @@ from onpolicy.utils.util import get_shape_from_obs_space
 
 from torch_geometric.data import Batch
 
+import numpy as np
+
 
 class R_Actor(nn.Module):
     """
@@ -78,15 +80,23 @@ class R_Actor(nn.Module):
             available_actions = check(available_actions).to(**self.tpdv)
 
         if self._use_gnn:
-            graphs = Batch.from_data_list(obs.flatten()).to(self.device, "x", "edge_attr", "edge_index")
-            graphs.edge_attr.requires_grad = True
+            obs_flat = obs.flatten()
+
+            graphs = Batch.from_data_list(obs_flat).to(self.device, "x", "edge_attr", "edge_index")
             actor_features = self.base(graphs.x, graphs.edge_attr, graphs.edge_index)
 
             # Restore the original shape.
             actor_features = actor_features.reshape(obs.shape[0], -1, actor_features.shape[-1])
+        
+            if hasattr(graphs, "agent_mask"):
+                agent_masks = np.array(graphs.agent_mask)
+                agent_masks = agent_masks.reshape(obs.shape[0], -1)
 
-            if hasattr(graphs, "agent_index"):
-                actor_features = actor_features[:, graphs.agent_index, :]
+                af = torch.zeros(obs.shape[0], obs.shape[1], actor_features.shape[-1])
+                for i in range(obs.shape[0]):
+                    af[i] = actor_features[i, agent_masks[i], :]
+                actor_features = af
+
             if self._use_gnn_mlp:
                 actor_features = self.mlp0(actor_features)
         else:
@@ -126,8 +136,8 @@ class R_Actor(nn.Module):
         if self._use_gnn:
             obs = check(obs).to(self.device, "x", "edge_attr", "edge_index")
             actor_features = self.base(obs.x, obs.edge_attr, obs.edge_index)
-            if hasattr(obs, "agent_index"):
-                actor_features = actor_features[obs.agent_index]
+            if hasattr(obs, "agent_idx"):
+                actor_features = actor_features[obs.agent_idx]
             if self._use_gnn_mlp:
                 actor_features = self.mlp0(actor_features)
         else:

patrolling_zoo/env/patrolling_zoo.py
@@ -649,7 +649,7 @@ class parallel_env(ParallelEnv):
             idx = subgraphNodes.index(f"agent_{agent.id}_pos")
             agent_mask = np.zeros(data.num_nodes, dtype=bool)
             agent_mask[idx] = True
-            data.agent_index = idx
+            data.agent_idx = idx
             data.agent_mask = agent_mask
 
             # Set up a numpy array to hold the observation.