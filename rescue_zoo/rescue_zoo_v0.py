a70bbf585904ccf0873baae0ae5feb407fe8e700
Anthony Goeckner <anthony.goeckner@northwestern.edu>
2024-03-18 10:37:33 -0500

    Deleted the unused rescue_zoo environment.

--- rescue_zoo/rescue_zoo_v0.py
+++
@@ -1,8 +0,0 @@
-from rescue_zoo.env.rescue_zoo import (
-    parallel_env
-)
-from rescue_zoo.env.graph_environment import (
-    GraphEnvironment
-)
-
-__all__ = ["parallel_env", "GraphEnvironment"]
